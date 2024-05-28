216383f95968cb27da3347fa8c37ac934a711f83
phoeni-cmd <bs162728@gmail.com>
2024-05-28 13:42:15 +0530

    fixed users

--- src/users/userController.ts
+++ src/users/userController.ts
@@ -1,7 +1,7 @@
-import { NextFunction, Request, Response } from "express";
+
+import { NextFunction, Request, Response, request } from "express";
 import createHttpError from "http-errors";
-import { userInfo } from "os";
-import userModel from "./userModel";
+import userModal from "./userModel";
 import bcrypt from "bcrypt";
 import { sign } from "jsonwebtoken";
 import { config } from "../config/config";
@@ -9,29 +9,38 @@ import { access } from "fs";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
-  //validation
+  //! validation
   if (!name || !email || !password) {
-    const error = createHttpError(400, "All fields are required");
+    const error = createHttpError(400, "All field are required");
     return next(error);
   }
-  //  Database call
-  const user = await userModel.findOne({ email });
+  //! database call.
+  const user = await userModal.findOne({
+    email: email,
+  });
 
-  /// password-> hash
+  if (user) {
+    const error = createHttpError(400, "User already exit with this email");
+    return next(error);
+  }
+
+  //! password
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  const newUser = userModel.create({
+  const newUser = await userModal.create({
     name,
     email,
     password: hashedPassword,
   });
-  //Token generation JWT
+  //! process
   const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
     expiresIn: "7d",
   });
 
-  //response
-
-  res.json({ accessToken: token });
+  //! response
+  res.json({
+    accessToken: token,
+  });
 };
+
 export { createUser };
