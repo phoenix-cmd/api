80f8c6532a16fa6b1867124f59d8e9f01679d231
phoeni-cmd <bs162728@gmail.com>
2024-06-05 15:58:59 +0530

    added book support

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,5 +1,7 @@
 
 import { Request,Response,NextFunction } from "express";
+import cloudinary from "../config/cloudinary";
+import path from "node:path";
 const createBook= async(
 
     req: Request, res: Response, next:NextFunction
@@ -7,6 +9,17 @@ const createBook= async(
 )=>{
 //const{}= req.body;
 console.log('files',req.files);
+const files= req.files as {[fieldname:string]:Express.Multer.File[]};
+// application/pdf
+const coverImageMimeType=files.coverImage [0].mimetype.split("/").at(-1);
+const fileName= files.coverImage[0].filename;
+const filePath=path.resolve(__dirname,'../../public/data/uploads',fileName);
+const uploadResult = await cloudinary.uploader.upload(filePath,{
+   filename_override: fileName,
+   folder:'book-covers',
+format: coverImageMimeType,
+});
+console.log('uploadResult',uploadResult);
 res.json({});
 
 
